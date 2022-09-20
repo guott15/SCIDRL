3cc3b664dbdb124b94b1dfc12256ca1953bfc800
guott15 <576289054@qq.com>
2022-09-20 21:01:19 +0800

    update

---
+++ scidrl/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+"""
+# Author: Tiantian Guo
+# File Name: __init__.py
+# Description:
+"""
+
+__author__ = "Tiantian Guo"
+__email__ = "guott18@mails.tsinghua.edu.cn"
+
+
+from .models import *
+from .main import SCIDRL_train
+
